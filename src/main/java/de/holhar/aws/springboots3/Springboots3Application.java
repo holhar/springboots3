f2caa6f12517344153938a2491bc2879f000a9cc
Holger Harms <holger.harms@adesso.de>
2022-06-08 17:09:44 +0200

    initial commit

---
+++ src/main/java/de/holhar/aws/springboots3/Springboots3Application.java
@@ -0,0 +1,13 @@
+package de.holhar.aws.springboots3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboots3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboots3Application.class, args);
+  }
+
+}
---
+++ src/test/java/de/holhar/aws/springboots3/Springboots3ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holhar.aws.springboots3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboots3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
